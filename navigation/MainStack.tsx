c39e5b15480f7b92cd221f2e538662dd61d8a236
CarlosMoreno <camc0308@gmail.com>
2024-08-25 00:54:37 -0500

    add User route props

--- navigation/MainStack.tsx
+++ navigation/MainStack.tsx
@@ -2,21 +2,50 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import UserList from "../screens/UserList";
+import UserDetail from "../screens/UserDetail";
 
-const Stack = createNativeStackNavigator();
+type Address = {
+  street: {
+    number: string;
+    name: string;
+  };
+  city: string;
+  state: string;
+  country: string;
+};
+
+type UserName = {
+  title: string;
+  first: string;
+  last: string;
+};
+
+export type RootStackParamList = {
+  userList: undefined;
+  userDetail: {
+    image: string;
+    name: UserName;
+    email: string;
+    phoneNumber: string;
+    address: Address;
+  };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const MainStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="user-list">
+      <Stack.Navigator initialRouteName="userList">
         <Stack.Screen
-          name="user-list"
+          name="userList"
           component={UserList}
-          navigationKey="user-list"
+          navigationKey="userList"
           options={{
             title: "Lista de usuarios",
           }}
         />
+        <Stack.Screen name="userDetail" component={UserDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
