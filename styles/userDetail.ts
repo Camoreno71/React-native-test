708098d6bd42827932148f4a0759b22a58e32dfd
CarlosMoreno <camc0308@gmail.com>
2024-08-25 01:12:10 -0500

    add user detail styles

---
+++ styles/userDetail.ts
@@ -0,0 +1,54 @@
+import { StyleSheet } from "react-native";
+
+export const userDetailStyles = StyleSheet.create({
+  container: {
+    paddingVertical: 48,
+    paddingHorizontal: 0,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  /** Profile */
+  profile: {
+    padding: 16,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "#e3e3e3",
+  },
+  userImageDetail: {
+    width: 150,
+    height: 150,
+    borderRadius: 9999,
+  },
+  profileName: {
+    marginTop: 12,
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#090909",
+  },
+  profileEmail: {
+    marginTop: 6,
+    fontSize: 16,
+    fontWeight: "400",
+    color: "#848484",
+  },
+  profileAction: {
+    marginTop: 12,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#007bff",
+    borderRadius: 12,
+  },
+  profileActionText: {
+    marginRight: 8,
+    fontSize: 15,
+    fontWeight: "600",
+    color: "#fff",
+  },
+});
